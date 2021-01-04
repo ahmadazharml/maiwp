cbb55993bfd3c44b4c389fd5c07c0bd3ddc351ec
Ahmad Azhar <76931906+ahmadazharml@users.noreply.github.com>
2021-01-04 16:04:00 +0800

    Update config.php

--- moja-app/config.php
+++ moja-app/config.php
@@ -5,7 +5,7 @@ $path = $_SERVER['SERVER_NAME'];
 //echo "Path: " . $path;
 
 $app = "moja-app"; // app
-$public = "/maiwp/";
+$public = "/moja/";
 
 define('APP', $app);
 define('BASEURL', 'http://' . $path . $public);
@@ -13,7 +13,7 @@ define('ACTVURL', 'create/email_activation/');
 define('TIMEZONE', date_default_timezone_set("Asia/Kuala_Lumpur"));
 
 // DB
-define('DB_HOST', 'sql101.epizy.com');
-define('DB_USER', 'epiz_27545162');
-define('DB_PASS', 'sI6w3kl9xw');
-define('DB_NAME', 'epiz_27545162_maiwp'); //crud-mvc
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', ''); //crud-mvc
